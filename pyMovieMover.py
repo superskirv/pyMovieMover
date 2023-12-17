3b492f8ff6143584bf56c19025be97bbaabef03f
superskirv <45948181+superskirv@users.noreply.github.com>
2023-12-16 22:23:54 -0800

    Update pyMovieMover.py
    -Added More information about the jobs in the que
    -Fixed some error messages, so it wont wait on users
    -Attempts to restart when an error occurs, still some issues cause it to hang.
    -Added scrollbar
    -Bug: Some buttons will break everything during transfers
    -Bug: Sometimes it will fail to copy and not delete the entry leading to it never deleting entries untill app restart.
    -Bug: Sometimes when it gets errors but continues and it completes the que it gets stuck in a loop.

--- pyMovieMover.py
+++ pyMovieMover.py
@@ -1,11 +1,11 @@
 import tkinter as tk
-from tkinter import filedialog, messagebox, ttk
-import os, re, threading, time, hashlib
+from tkinter import ttk, Scrollbar, filedialog, messagebox
+import os, re, threading, time
 from queue import Queue
-
+#import hashlib
 #
-#   Version 1.0a
-#   Last Update: 2023nov17
+#   Version 1.2b
+#   Last Update: 2023dec16
 #   Created by: ChatGPT4 and Super.Skirv
 #
 
@@ -14,26 +14,25 @@ global file_types, user_destinations
 #String probably needs double backwards slashs(on windows), maybe linux also.
 #Relative paths probably work here also.
 #You can add as many locations here as you want. They just wont work if they dont meet the above requirements. Unless you change things...
-user_destinations = ["L:\\plex\\movies", "L:\\plex\\shows", "L:\\plex\\anime"]
+user_destinations = ["Z:\\plex\\movies", "Z:\\plex\\shows", "Z:\\plex\\anime"]
 #User defined file types to move.
 file_types = [".mp4", ".avi", ".mkv"]
 
 class FileCopier:
-    def __init__(self, queue, listbox, current_dest_label):
+    def __init__(self, queue, tree, current_dest_label):
         self.queue = queue
-        self.listbox = listbox
+        self.tree = tree  # Updated reference to tree
         self.current_dest_label = current_dest_label
         self.stop_copy = False
         self.currently_copying = False
         self.paused = False
         self.kill_copy = False
-
+        self.retry = 0
     def copy_next_file(self):
         if not self.queue.empty():
             self.currently_copying = True
             src, dest = self.queue.get()
             threading.Thread(target=self.copy_file_thread, args=(src, dest)).start()
-
     def get_file_md5(self, file_path):
         # Calculate the MD5 hash of a file
         md5 = hashlib.md5()
@@ -44,7 +43,6 @@ class FileCopier:
                     break
                 md5.update(data)
         return md5.hexdigest()
-
     def get_subfolder(self, src, dest):
         file_name = os.path.basename(src)
         dir_type = os.path.basename(dest)
@@ -58,9 +56,17 @@ class FileCopier:
             show_name = self.get_show_name(file_name)
             season_num = self.get_season(file_name)
 
-            if not show_name:
+            # print('show_name: ',show_name)
+            # print('season_num: ',season_num)
+            # print('file_name: ',file_name)
+
+            season_name = None
+            if "extra" in file_name:
+                season_name = 'Extras'
+                season_num = None
+            elif not show_name:
                 return False
-            if season_num:
+            elif season_num:
                 season_name = 'Season ' + season_num
             else:
                 return False
@@ -77,40 +83,48 @@ class FileCopier:
             return "#"
         else:
             return subfolder
-
     def get_season(self, string):
-        pattern = r'S(\d+)'
+        pattern = r'^(.*?)(?:S(\d{2})E\d{2}|s(\d{2})e\d{2})'
         match = re.search(pattern, string)
-
         if match:
-            return match.group(1)
+            season_group = match.group(2) or match.group(3)
+            season = season_group if season_group else None
+            return season
         else:
             return False
-
     def get_show_name(self, string):
-        pattern = r'^(.*?)(?:S\d{2}E\d{2})'
-        match = re.search(pattern, file_name)
-
+        pattern = r'^(.*?)(?:S(\d{2})E\d{2}|s(\d{2})e\d{2}|S(\d{2})E\d{2}-E\d{2}|s(\d{2})e\d{2}-e\d{2})'
+        match = re.search(pattern, string)
+        #print("--match: ",match)
         if match:
-            show_name = match.group(1)
-            formatted_show_name = ' '.join(word.capitalize() for word in show_name.split('.'))
-            stripped_string = formatted_show_name.strip()
+            show_name_group = match.group(1)
+            show_name = ' '.join(word.capitalize() for word in show_name_group.split('.'))
+            stripped_string = show_name.rstrip('-').strip()
             return stripped_string
         else:
-            return False
-
+            pattern = r'^(.*?)(?:S\d{2})'
+            match = re.search(pattern, string)
+            #print("--match: ",match)
+            if match:
+                show_name = match.group(1)
+                formatted_show_name = ' '.join(word.capitalize() for word in show_name.split('.'))      #Need to search for underscores and spaces as well as periods.
+                stripped_string = show_name.rstrip('-').strip()
+                return stripped_string
+            else:
+                return "0000-Show-Name"
     def copy_file_thread(self, src, dest):
         self.stop_copy = False
         self.currently_copying = True
         self.kill_copy = False
         self.paused = False
+        dest_loc = dest
         # Create the subfolder based on the first letter of the file name
         subfolder = self.get_subfolder(src, dest)
         if not subfolder:
             self.update_gui_error(f"Could not determine where to put file. File/Destination name might be weird. {src}")
             self.kill_copy = True
         dest_folder = os.path.join(dest, subfolder)
-        self.set_current_destination_label(dest_folder)
+        self.set_current_destination_label(os.path.join(dest_folder, os.path.basename(src)))
 
         # Check and create destination folder if it doesn't exist
         if not os.path.exists(dest_folder):
@@ -159,33 +173,54 @@ class FileCopier:
                         eta = (file_size - copied) / speed if speed > 0 else 0
                         self.update_gui_progress(copied / file_size, speed, eta, dest)
                         percent = copied / file_size
+            except PermissionError as e:
+                self.retry += 1
+                if self.retry > 3:
+                    print(f"An Permission Error occurred: {e}\nDeleting partial file and skipping.", dest)
+                    os.remove(dest)
+                else:
+                    print(f"An Permission Error occurred: \nRetring file {3-self.retry} more times before skipping.", dest)
+                    os.remove(dest)
             except Exception as e:
-                self.update_gui_error(f"An error occurred: {e}")
-                os.remove(dest)
+                self.retry += 1
+                if self.retry > 3:
+                    #self.update_gui_error(f"Current file will be deleted. An error occurred: {e}")
+                    print(f"An error occurred: {e}\nCurrent file will be deleted.", dest)
+                    os.remove(dest)
+                else:
+                    #self.update_gui_error(f"An error occurred: {e}\nRetring file 3 times before skiping.")
+                    print(f"An error occurred: \nRetring file {3-self.retry} more times before skipping.", dest)
+                    os.remove(dest)
+        if self.retry <= 0 or self.retry > 3:
+            children = self.tree.get_children()
+            if children:
+                self.tree.delete(children[0])
+            self.retry = 0
+        else:
+            subfolder = copier.get_subfolder(file, dest_loc)
+            tree.insert("", "end", values=(os.path.basename(file), os.path.join(dest_loc, subfolder) if subfolder else dest_loc))
+            self.queue.put((file, dest_loc))
 
-        self.listbox.delete(0)
         self.currently_copying = False
         self.copy_next_file()
-
     def update_gui_progress(self, ratio, speed, eta, dest):
         percentage = int(ratio * 100)
         progress_var.set(percentage)
         speed_label.config(text=f"Speed: {speed / 1024 / 1024:.2f} MB/s")
         eta_label.config(text=f"ETA: {eta:.1f}s")
 
+        # Update the Treeview with file information
+        file_info = os.path.basename(dest)
+        dest_folder = os.path.dirname(dest)
     def update_gui_error(self, message):
         messagebox.showerror("Error", message)
-
     def skip_current_file(self):
         if self.currently_copying:
             self.stop_copy = True
-
     def toggle_pause(self):
         self.paused = not self.paused
-
     def set_current_destination_label(self, dest):
         self.current_dest_label.config(text=f"{dest}")
-
 def find_movie_files(directory, extensions=None):
     global file_types
     if extensions is None:
@@ -197,7 +232,6 @@ def find_movie_files(directory, extensions=None):
             if file.lower().endswith(tuple(extensions)):
                 movie_files.append(os.path.join(root, file))
     return movie_files
-
 def add_to_queue():
     source_dir = source_entry.get()
     if not source_dir:
@@ -216,9 +250,9 @@ def add_to_queue():
         return
 
     for file in files_to_copy:
-        queue.put((file, dest))  # Use the selected destination folder
-        listbox.insert(tk.END, os.path.basename(file))
-
+        queue.put((file, dest))
+        subfolder = copier.get_subfolder(file, dest)
+        tree.insert("", "end", values=(os.path.basename(file), os.path.join(dest, subfolder) if subfolder else dest))
 def start_copying():
     if not dest_entry.get():
         messagebox.showerror("Error", "Please select a destination directory.")
@@ -228,38 +262,22 @@ def start_copying():
         return
     if not copier.currently_copying:
         copier.copy_next_file()
-
 def delete_selected():
-    selected = listbox.curselection()
-    index_to_delete = selected[0] if selected else 0  # Use the selected index, or default to 0
-
-    if listbox.size() > 0:  # Check if there's at least one item in the list
-        listbox.delete(index_to_delete)
-        remove_from_queue(index_to_delete)
-    else:
-        messagebox.showinfo("Info", "No files in the queue to delete.")
-
+    selected_item = tree.selection()
+    if selected_item:
+        tree.delete(selected_item[0])
+        remove_from_queue(selected_item[0])
+def stop_copy():
+    copier.stop_copy = True
 def clear_queue():
-    # Create a new queue with the currently running job and replace the global queue
-    if listbox.size() > 1:
-        #To lazy to figure out why the for loop only does half the list box.
-        while listbox.size() > 1:
-            num = listbox.size()
-            for index in range(num):
-                if index != 0:
-                    listbox.delete(index)
-                    remove_from_queue(index)
-    elif listbox.size() == 1:
-        delete_selected()
+    if tree.get_children():
+        tree.delete(*tree.get_children())
+        remove_from_queue()
+def remove_from_queue(index=None):
+    if index is not None:
+        queue.queue.queue = [item for i, item in enumerate(queue.queue.queue) if i != index]
     else:
-        messagebox.showinfo("Info", "No files in the queue to delete.")
-def remove_from_queue(index):
-    new_queue = Queue()
-    for i, item in enumerate(list(queue.queue)):
-        if i != index:
-            new_queue.put(item)
-    queue.queue.clear()
-    queue.queue = new_queue.queue
+        queue.queue.queue.clear()
 
 app = tk.Tk()
 app.title("Movie File Copier")
@@ -295,11 +313,28 @@ dest_entry.pack(side=tk.LEFT, expand=True, fill=tk.X)
 queue_list_frame = tk.Frame(queue_main_frame)
 queue_list_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
-listbox = tk.Listbox(queue_list_frame)
-listbox.pack(fill=tk.BOTH, expand=True)
+# Create a vertical scrollbar
+tree_scrollbar = Scrollbar(queue_list_frame, orient="vertical")
 
-# Update copier with listbox reference
-copier.listbox = listbox
+# Create the Treeview with the vertical scrollbar
+tree = ttk.Treeview(
+    queue_list_frame,
+    columns=("File", "Destination"),
+    show="headings",
+    selectmode="browse",
+    yscrollcommand=tree_scrollbar.set  # Link the Treeview to the scrollbar
+)
+tree.heading("File", text="File")
+tree.heading("Destination", text="Destination")
+tree.column("File", anchor="w", width=200)
+tree.column("Destination", anchor="w", width=400)
+tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+# Set the scrollbar command to update the view
+tree_scrollbar.config(command=tree.yview)
+tree_scrollbar.pack(side=tk.RIGHT, fill="y")
+
+copier = FileCopier(queue, tree, current_destination_label)
 
 # Queue Button Frame
 queue_button_frame = tk.Frame(queue_main_frame)
@@ -312,6 +347,9 @@ start_button = tk.Button(queue_button_frame, text="Start Copying", command=start
 start_button.pack()
 
 pause_button = tk.Button(queue_button_frame, text="Pause/Resume", command=copier.toggle_pause)
+pause_button.pack()
+
+pause_button = tk.Button(queue_button_frame, text="Stop", command=stop_copy)
 pause_button.pack()
 
 skip_button = tk.Button(queue_button_frame, text="Skip Current File", command=copier.skip_current_file)
